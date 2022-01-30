bcabe49ae93ae2cd9395d730273f35b321e72d64
LukeSchaffel <lukeschaffel@gmail.com>
2022-01-29 21:32:54 -0500

    comple functional game

--- app.js
+++ app.js
@@ -8,58 +8,38 @@ const game = {
 
 
   getGuess: function(){
-     
-    let input = null
-    
-    
-      input = window.prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-      this.playerCurrentGuess = parseInt(input)
-  
-    console.log(typeof this.playerCurrentGuess)
+    let input = null 
+    input = window.prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+    this.playerCurrentGuess = parseInt(input)
     return this.playerCurrentGuess
     },
   
   render: function(){
     if (this.playerCurrentGuess === this.secretNum){
       console.log(`Congrats! You guessed the number in ${this.prevoiusGuesses.length} guesses!`) 
-    } else { console.log(`Your guess is too [high|low] Previous guesses: x, x, x, x`)}
-
-
+    } else if (this.playerCurrentGuess > this.secretNum){
+      console.log(`Your guess is too high Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
+    } else console.log(`Your guess is too low! Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
   },
   
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-      
-      
         while (this.prevoiusGuesses[this.prevoiusGuesses.length-1] !== this.secretNum){
-          this.getGuess()
-        console.log(`the computer picked ${this.secretNum}`)
-        console.log (this.playerCurrentGuess)
+        this.getGuess()
+        //console.log(`the computer picked ${this.secretNum}`)
+        //console.log (this.playerCurrentGuess)
         this.prevoiusGuesses.push(this.playerCurrentGuess)
-        console.log(this.prevoiusGuesses)
+        //console.log(this.prevoiusGuesses)
         this.render()
       }
-          
-      
-      
-        
-      //return this.prevoiusGuesses
-      
-      
-      // while ()
-      // {this.getGuess()
-      // this.prevoiusGuesses.push(this.playerCurrentGuess)}
-
-      
-
   },
   
 
   
   }
 
-  game.play()
+game.play()
 
- console.log(game.play)
+
   
