a732c91fc06e2f8b97b62ea0d70533c40eec18ee
LukeSchaffel <lukeschaffel@gmail.com>
2022-01-30 14:28:26 -0500

    add better prompts to final draft

--- app.js
+++ app.js
@@ -9,17 +9,37 @@ const game = {
 
   getGuess: function(){
     let input = null 
-    input = window.prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-    this.playerCurrentGuess = parseInt(input)
-    return this.playerCurrentGuess
+    let isCorrectNum = null
+
+    while (this.playerCurrentGuess !== this.secretNum ) {
+
+
+          if (this.prevoiusGuesses.length ===0) {
+            input = window.prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}.`)
+            this.playerCurrentGuess = parseInt(input)
+            return this.playerCurrentGuess 
+            
+          } else { input = window.prompt(`Enter a number  between ${this.smallestNum} and ${this.biggestNum}. Previous Guesses = ${this.prevoiusGuesses.join(', ')}`)
+          this.playerCurrentGuess = parseInt(input)
+          return this.playerCurrentGuess 
+        
+      }
+      
+     
+    }
+    
     },
   
   render: function(){
+    
     if (this.playerCurrentGuess === this.secretNum){
-      console.log(`Congrats! You guessed the number in ${this.prevoiusGuesses.length} guesses!`) 
+      //console.log(`Congrats! You guessed the number in ${this.prevoiusGuesses.length} guesses!`) 
+      alert(`Congrats! You guessed the number in ${this.prevoiusGuesses.length} guesses!`)
     } else if (this.playerCurrentGuess > this.secretNum){
-      console.log(`Your guess is too high Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
-    } else console.log(`Your guess is too low! Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
+      //console.log(`Your guess is too high Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
+      alert(`Your guess is too high! Previous guesses: ${this.prevoiusGuesses.join(', ')} `)
+    } else alert( `Your guess is too low! Previous guesses: ${this.prevoiusGuesses.join(', ')}`)
+    //console.log(`Your guess is too low! Previous guesses: ${this.prevoiusGuesses.join(', ')}`) 
   },
   
   play: function() {
@@ -41,7 +61,4 @@ const game = {
 
 
 
-  game.play()
-
-
-  
+ game.play()
